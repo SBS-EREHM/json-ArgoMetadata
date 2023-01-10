e25a4fa19d65883bbda5408349a88a62921945f1
Eric Rehm <68403249+SBS-EREHM@users.noreply.github.com>
2023-01-10 15:54:23 -0800

    Change to "with" statement paradigm for file open

--- code/JSONtest.py
+++ code/JSONtest.py
@@ -3,26 +3,23 @@ import json
 # files to test
 # fname = 'sensor-DRUCK-DRUCK_2900PSIA-11698373.json'
 # fname = 'sensor-AANDERAA-AANDERAA_OPTODE_3830-498.json'
-# fname = 'sensor-SBE-SBE41CP-13875.json'
-fname = 'sensor-WETLABS-MCOMS_FLBBCDX-0157.json'
+fname = 'sensor-SBE-SBE41CP-13875.json'
+# fname = 'sensor-WETLABS-MCOMS_FLBBCDX-0157.json'
 
 # Open JSON file
-f = open(fname)
-  
-# returns JSON object as a dictionary
-data = json.load(f)
-  
-# Iterate through portions of the json list
-print('SENSOR:')
-for i in data['argo'][0]['SENSOR']:
-    print(i)
+with open('examples/'+fname) as f:
 
-print('\nPARAMETER:')
-for i in data['argo'][1]['PARAMETER']:
-    print(i)
+    # returns JSON object as a dictionary
+    data = json.load(f)
+    
+    # Iterate through portions of the json list
+    print('SENSOR:')
+    for i in data['argo'][0]['SENSOR']:
+        print(i)
 
-print('\nvendor:')
-print(data['vendor'][0]['SENSOR_MAKER'])
+    print('\nPARAMETER:')
+    for i in data['argo'][1]['PARAMETER']:
+        print(i)
 
-# Close file
-f.close()
+    print('\nvendor:')
+    print(data['vendor'][0]['SENSOR_MAKER'])
