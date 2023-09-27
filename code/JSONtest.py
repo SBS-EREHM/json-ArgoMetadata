7aedafcea2ef09cc9f75099cd8699060b2892cad
Eric Rehm <68403249+SBS-EREHM@users.noreply.github.com>
2023-09-27 12:25:33 -0700

    Update a bit before move to euroargodev

--- code/JSONtest.py
+++ code/JSONtest.py
@@ -1,11 +1,9 @@
 import json
 
 # files to test
-# fname = 'sensor-DRUCK-DRUCK_2900PSIA-11698373.json'
 # fname = 'sensor-AANDERAA-AANDERAA_OPTODE_3830-498.json'
-# fname = 'sensor-SBE-SBE41CP-13875.json'
 fname = 'sensor-SBE-SBE41CP-11643.json'
-# fname = 'sensor-WETLABS-MCOMS_FLBBCDX-0157.json'
+fname = 'sensor-WETLABS-MCOMS_FLBBCD-0157.json'
 
 # Open JSON file
 with open('examples/'+fname) as f:
@@ -15,28 +13,20 @@ with open('examples/'+fname) as f:
 
     print('sensor_info:')
     for key, value in data['sensor_info'].items():
-        print(key, value)
+        print(f'{key}\t{value}')
     
     # Iterate through portions of the json list
     print('sensors:')
     for sensor in data['SENSORS']:
         for key, value in sensor.items() :
-            print(key, value)
-        # print(sensor['SENSOR'][:])
-        # print(sensor['SENSOR_MAKER'][:])
+            print(f'{key}\t{value}')
 
     print('parameters:')
     for param in data['PARAMETERS']:
         for key, value in param.items() :
             if key != 'PREDEPLOYMENT_CALIB_COEFFICIENT_LIST' :
-                print(key, value)
+                print(f'{key}\t{value}')
 
         print('REDEPLOYMENT_CALIB_COEFFICIENT_LIST:')
         for key, calcoef in param['PREDEPLOYMENT_CALIB_COEFFICIENT_LIST'].items():
-            print(key, calcoef)
-
-
-1
-
-    # print('\nvendor:')
-    # print(data['vendor'][0]['SENSOR_MAKER'])
+            print(f'\t{key}\t{calcoef}')
